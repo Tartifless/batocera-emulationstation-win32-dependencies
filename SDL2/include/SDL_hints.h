895182b0542a2d21d5d6c4c4b00082f9608eefd9
Benoit S. <tartifless@gmail.com>
2025-09-07 02:43:08 +0200

    Revert "Update SDL_hints.h"
    This reverts commit 8c8da7ab3c226626e4f4471dea4206ca9d1bea11.

--- SDL2/include/SDL_hints.h
+++ SDL2/include/SDL_hints.h
@@ -590,111 +590,6 @@ extern "C" {
  */
 #define SDL_HINT_JOYSTICK_ALLOW_BACKGROUND_EVENTS "SDL_JOYSTICK_ALLOW_BACKGROUND_EVENTS"
 
- /**
-  * A variable containing a list of arcade stick style controllers.
-  *
-  * The format of the string is a comma separated list of USB VID/PID pairs in
-  * hexadecimal form, e.g.
-  *
-  * 0xAAAA/0xBBBB,0xCCCC/0xDDDD
-  *
-  * The variable can also take the form of @file, in which case the named file
-  * will be loaded and interpreted as the value of the variable.
-  */
-#define SDL_HINT_JOYSTICK_ARCADESTICK_DEVICES "SDL_JOYSTICK_ARCADESTICK_DEVICES"
-
-  /**
-   * A variable containing a list of devices that are not arcade stick style
-   * controllers.
-   *
-   * This will override SDL_HINT_JOYSTICK_ARCADESTICK_DEVICES and the built in
-   * device list.
-   *
-   * The format of the string is a comma separated list of USB VID/PID pairs in
-   * hexadecimal form, e.g.
-   *
-   * 0xAAAA/0xBBBB,0xCCCC/0xDDDD
-   *
-   * The variable can also take the form of @file, in which case the named file
-   * will be loaded and interpreted as the value of the variable.
-   */
-#define SDL_HINT_JOYSTICK_ARCADESTICK_DEVICES_EXCLUDED "SDL_JOYSTICK_ARCADESTICK_DEVICES_EXCLUDED"
-
-   /**
-    * A variable containing a list of devices that should not be considerd
-    * joysticks.
-    *
-    * The format of the string is a comma separated list of USB VID/PID pairs in
-    * hexadecimal form, e.g.
-    *
-    * 0xAAAA/0xBBBB,0xCCCC/0xDDDD
-    *
-    * The variable can also take the form of @file, in which case the named file
-    * will be loaded and interpreted as the value of the variable.
-    */
-#define SDL_HINT_JOYSTICK_BLACKLIST_DEVICES "SDL_JOYSTICK_BLACKLIST_DEVICES"
-
-    /**
-     * A variable containing a list of devices that should be considered
-     * joysticks.
-     *
-     * This will override SDL_HINT_JOYSTICK_BLACKLIST_DEVICES and the built in
-     * device list.
-     *
-     * The format of the string is a comma separated list of USB VID/PID pairs in
-     * hexadecimal form, e.g.
-     *
-     * 0xAAAA/0xBBBB,0xCCCC/0xDDDD
-     *
-     * The variable can also take the form of @file, in which case the named file
-     * will be loaded and interpreted as the value of the variable.
-     */
-#define SDL_HINT_JOYSTICK_BLACKLIST_DEVICES_EXCLUDED "SDL_JOYSTICK_BLACKLIST_DEVICES_EXCLUDED"
-
-     /**
-      * A variable containing a list of flightstick style controllers.
-      *
-      * The format of the string is a comma separated list of USB VID/PID pairs in
-      * hexadecimal form, e.g.
-      *
-      * 0xAAAA/0xBBBB,0xCCCC/0xDDDD
-      *
-      * The variable can also take the form of @file, in which case the named file
-      * will be loaded and interpreted as the value of the variable.
-      */
-#define SDL_HINT_JOYSTICK_FLIGHTSTICK_DEVICES "SDL_JOYSTICK_FLIGHTSTICK_DEVICES"
-
-      /**
-       * A variable containing a list of devices that are not flightstick style
-       * controllers.
-       *
-       * This will override SDL_HINT_JOYSTICK_FLIGHTSTICK_DEVICES and the built in
-       * device list.
-       *
-       * The format of the string is a comma separated list of USB VID/PID pairs in
-       * hexadecimal form, e.g.
-       *
-       * 0xAAAA/0xBBBB,0xCCCC/0xDDDD
-       *
-       * The variable can also take the form of @file, in which case the named file
-       * will be loaded and interpreted as the value of the variable.
-       */
-#define SDL_HINT_JOYSTICK_FLIGHTSTICK_DEVICES_EXCLUDED "SDL_JOYSTICK_FLIGHTSTICK_DEVICES_EXCLUDED"
-
-       /**
-        * A variable containing a list of devices known to have a GameCube form
-        * factor.
-        *
-        * The format of the string is a comma separated list of USB VID/PID pairs in
-        * hexadecimal form, e.g.
-        *
-        * 0xAAAA/0xBBBB,0xCCCC/0xDDDD
-        *
-        * The variable can also take the form of @file, in which case the named file
-        * will be loaded and interpreted as the value of the variable.
-        */
-#define SDL_HINT_JOYSTICK_GAMECUBE_DEVICES "SDL_JOYSTICK_GAMECUBE_DEVICES"
-
 /**
  *  \brief  A variable controlling whether the HIDAPI joystick drivers should be used.
  *
@@ -718,24 +613,6 @@ extern "C" {
 #define SDL_HINT_JOYSTICK_HIDAPI_GAMECUBE "SDL_JOYSTICK_HIDAPI_GAMECUBE"
 
  /**
-  * A variable controlling whether "low_frequency_rumble" and
-  * "high_frequency_rumble" is used to implement the GameCube controller's 3
-  * rumble modes, Stop(0), Rumble(1), and StopHard(2) this is useful for
-  * applications that need full compatibility for things like ADSR envelopes.
-  *
-  * Stop is implemented by setting "low_frequency_rumble" to "0" and
-  * "high_frequency_rumble" ">0" Rumble is both at any arbitrary value,
-  * StopHard is implemented by setting both "low_frequency_rumble" and
-  * "high_frequency_rumble" to "0"
-  *
-  * This variable can be set to the following values:
-  *
-  * - "0": Normal rumble behavior is behavior is used (default)
-  * - "1": Proper GameCube controller rumble behavior is used
-  */
-#define SDL_HINT_JOYSTICK_GAMECUBE_RUMBLE_BRAKE "SDL_JOYSTICK_GAMECUBE_RUMBLE_BRAKE"
-
- /**
   *  \brief  A variable controlling whether Switch Joy-Cons should be treated the same as Switch Pro Controllers when using the HIDAPI driver.
   *
   *  This variable can be set to the following values:
@@ -745,33 +622,6 @@ extern "C" {
   *  This does not combine Joy-Cons into a single controller. That's up to the user.
   */
 #define SDL_HINT_JOYSTICK_HIDAPI_JOY_CONS "SDL_JOYSTICK_HIDAPI_JOY_CONS"
-
-  /**
-   * A variable controlling whether Nintendo Switch Joy-Con controllers will be
-   * combined into a single Pro-like controller when using the HIDAPI driver
-   *
-   * This variable can be set to the following values:
-   *
-   * - "0": Left and right Joy-Con controllers will not be combined and each
-   *   will be a mini-gamepad
-   * - "1": Left and right Joy-Con controllers will be combined into a single
-   *   controller (the default)
-   */
-#define SDL_HINT_JOYSTICK_HIDAPI_COMBINE_JOY_CONS "SDL_JOYSTICK_HIDAPI_COMBINE_JOY_CONS"
-
-   /**
-    * A variable controlling whether Nintendo Switch Joy-Con controllers will be
-    * in vertical mode when using the HIDAPI driver
-    *
-    * This variable can be set to the following values:
-    *
-    * - "0": Left and right Joy-Con controllers will not be in vertical mode (the
-    *   default)
-    * - "1": Left and right Joy-Con controllers will be in vertical mode
-    *
-    * This hint must be set before calling SDL_Init(SDL_INIT_GAMECONTROLLER)
-    */
-#define SDL_HINT_JOYSTICK_HIDAPI_VERTICAL_JOY_CONS "SDL_JOYSTICK_HIDAPI_VERTICAL_JOY_CONS"
 
  /**
   *  \brief  A variable controlling whether the HIDAPI driver for Amazon Luna controllers connected via Bluetooth should be used.
@@ -783,50 +633,6 @@ extern "C" {
   *  The default is the value of SDL_HINT_JOYSTICK_HIDAPI
   */
 #define SDL_HINT_JOYSTICK_HIDAPI_LUNA "SDL_JOYSTICK_HIDAPI_LUNA"
-
-  /**
-   * A variable controlling whether the HIDAPI driver for Nintendo Online
-   * classic controllers should be used.
-   *
-   * This variable can be set to the following values:
-   *
-   * - "0": HIDAPI driver is not used
-   * - "1": HIDAPI driver is used
-   *
-   * The default is the value of SDL_HINT_JOYSTICK_HIDAPI
-   */
-#define SDL_HINT_JOYSTICK_HIDAPI_NINTENDO_CLASSIC "SDL_JOYSTICK_HIDAPI_NINTENDO_CLASSIC"
-
-   /**
-    * A variable controlling whether the HIDAPI driver for NVIDIA SHIELD
-    * controllers should be used.
-    *
-    * This variable can be set to the following values:
-    *
-    * - "0": HIDAPI driver is not used
-    * - "1": HIDAPI driver is used
-    *
-    * The default is the value of SDL_HINT_JOYSTICK_HIDAPI
-    */
-#define SDL_HINT_JOYSTICK_HIDAPI_SHIELD "SDL_JOYSTICK_HIDAPI_SHIELD"
-
-    /**
-     * A variable controlling whether the HIDAPI driver for PS3 controllers should
-     * be used.
-     *
-     * This variable can be set to the following values:
-     *
-     * - "0": HIDAPI driver is not used
-     * - "1": HIDAPI driver is used
-     *
-     * The default is the value of SDL_HINT_JOYSTICK_HIDAPI on macOS, and "0" on
-     * other platforms.
-     *
-     * It is not possible to use this driver on Windows, due to limitations in the
-     * default drivers installed. See https://github.com/ViGEm/DsHidMini for an
-     * alternative driver on Windows.
-     */
-#define SDL_HINT_JOYSTICK_HIDAPI_PS3 "SDL_JOYSTICK_HIDAPI_PS3"
 
 /**
  *  \brief  A variable controlling whether the HIDAPI driver for PS4 controllers should be used.
@@ -921,19 +727,6 @@ extern "C" {
  */
 #define SDL_HINT_JOYSTICK_HIDAPI_STEAM "SDL_JOYSTICK_HIDAPI_STEAM"
 
- /**
-  * A variable controlling whether the HIDAPI driver for the Steam Deck builtin
-  * controller should be used.
-  *
-  * This variable can be set to the following values:
-  *
-  * - "0": HIDAPI driver is not used
-  * - "1": HIDAPI driver is used
-  *
-  * The default is the value of SDL_HINT_JOYSTICK_HIDAPI
-  */
-#define SDL_HINT_JOYSTICK_HIDAPI_STEAMDECK "SDL_JOYSTICK_HIDAPI_STEAMDECK"
-
 /**
  *  \brief  A variable controlling whether the HIDAPI driver for Nintendo Switch controllers should be used.
  *
@@ -956,57 +749,6 @@ extern "C" {
  */
 #define SDL_HINT_JOYSTICK_HIDAPI_SWITCH_HOME_LED "SDL_JOYSTICK_HIDAPI_SWITCH_HOME_LED"
 
- /**
-  * A variable controlling whether the Home button LED should be turned on when
-  * a Nintendo Switch Joy-Con controller is opened
-  *
-  * This variable can be set to the following values:
-  *
-  * - "0": home button LED is turned off
-  * - "1": home button LED is turned on
-  *
-  * By default the Home button LED state is not changed. This hint can also be
-  * set to a floating point value between 0.0 and 1.0 which controls the
-  * brightness of the Home button LED.
-  */
-#define SDL_HINT_JOYSTICK_HIDAPI_JOYCON_HOME_LED "SDL_JOYSTICK_HIDAPI_JOYCON_HOME_LED"
-
-  /**
-   * A variable controlling whether the player LEDs should be lit to indicate
-   * which player is associated with a Nintendo Switch controller.
-   *
-   * This variable can be set to the following values:
-   *
-   * - "0": player LEDs are not enabled
-   * - "1": player LEDs are enabled (the default)
-   */
-#define SDL_HINT_JOYSTICK_HIDAPI_SWITCH_PLAYER_LED "SDL_JOYSTICK_HIDAPI_SWITCH_PLAYER_LED"
-
-   /**
-    * A variable controlling whether the HIDAPI driver for Nintendo Wii and Wii U
-    * controllers should be used.
-    *
-    * This variable can be set to the following values:
-    *
-    * - "0": HIDAPI driver is not used
-    * - "1": HIDAPI driver is used
-    *
-    * This driver doesn't work with the dolphinbar, so the default is SDL_FALSE
-    * for now.
-    */
-#define SDL_HINT_JOYSTICK_HIDAPI_WII "SDL_JOYSTICK_HIDAPI_WII"
-
-    /**
-     * A variable controlling whether the player LEDs should be lit to indicate
-     * which player is associated with a Wii controller.
-     *
-     * This variable can be set to the following values:
-     *
-     * - "0": player LEDs are not enabled
-     * - "1": player LEDs are enabled (the default)
-     */
-#define SDL_HINT_JOYSTICK_HIDAPI_WII_PLAYER_LED "SDL_JOYSTICK_HIDAPI_WII_PLAYER_LED"
-
 /**
  *  \brief  A variable controlling whether the HIDAPI driver for XBox controllers should be used.
  *
@@ -1017,93 +759,6 @@ extern "C" {
  *  The default is "0" on Windows, otherwise the value of SDL_HINT_JOYSTICK_HIDAPI
  */
 #define SDL_HINT_JOYSTICK_HIDAPI_XBOX   "SDL_JOYSTICK_HIDAPI_XBOX"
-
-/**
- * A variable controlling whether the HIDAPI driver for XBox 360 controllers
- * should be used.
- *
- * This variable can be set to the following values:
- *
- * - "0": HIDAPI driver is not used
- * - "1": HIDAPI driver is used
- *
- * The default is the value of SDL_HINT_JOYSTICK_HIDAPI_XBOX
- */
-#define SDL_HINT_JOYSTICK_HIDAPI_XBOX_360   "SDL_JOYSTICK_HIDAPI_XBOX_360"
-
-/**
- * A variable controlling whether the player LEDs should be lit to indicate
- * which player is associated with an Xbox 360 controller.
- *
- * This variable can be set to the following values:
- *
- * - "0": player LEDs are not enabled
- * - "1": player LEDs are enabled (the default)
- */
-#define SDL_HINT_JOYSTICK_HIDAPI_XBOX_360_PLAYER_LED "SDL_JOYSTICK_HIDAPI_XBOX_360_PLAYER_LED"
-
-/**
- * A variable controlling whether the HIDAPI driver for XBox 360 wireless
- * controllers should be used.
- *
- * This variable can be set to the following values:
- *
- * - "0": HIDAPI driver is not used
- * - "1": HIDAPI driver is used
- *
- * The default is the value of SDL_HINT_JOYSTICK_HIDAPI_XBOX_360
- */
-#define SDL_HINT_JOYSTICK_HIDAPI_XBOX_360_WIRELESS   "SDL_JOYSTICK_HIDAPI_XBOX_360_WIRELESS"
-
-/**
- * A variable controlling whether the HIDAPI driver for XBox One controllers
- * should be used.
- *
- * This variable can be set to the following values:
- *
- * - "0": HIDAPI driver is not used
- * - "1": HIDAPI driver is used
- *
- * The default is the value of SDL_HINT_JOYSTICK_HIDAPI_XBOX
- */
-#define SDL_HINT_JOYSTICK_HIDAPI_XBOX_ONE   "SDL_JOYSTICK_HIDAPI_XBOX_ONE"
-
-/**
- * A variable controlling whether the Home button LED should be turned on when
- * an Xbox One controller is opened
- *
- * This variable can be set to the following values:
- *
- * - "0": home button LED is turned off
- * - "1": home button LED is turned on
- *
- * By default the Home button LED state is not changed. This hint can also be
- * set to a floating point value between 0.0 and 1.0 which controls the
- * brightness of the Home button LED. The default brightness is 0.4.
- */
-#define SDL_HINT_JOYSTICK_HIDAPI_XBOX_ONE_HOME_LED "SDL_JOYSTICK_HIDAPI_XBOX_ONE_HOME_LED"
-
-/**
- * A variable controlling whether IOKit should be used for controller
- * handling.
- *
- * This variable can be set to the following values:
- *
- * - "0": IOKit is not used
- * - "1": IOKit is used (the default)
- */
-#define SDL_HINT_JOYSTICK_IOKIT "SDL_JOYSTICK_IOKIT"
-
-/**
- * A variable controlling whether GCController should be used for controller
- * handling.
- *
- * This variable can be set to the following values:
- *
- * - "0": GCController is not used
- * - "1": GCController is used (the default)
- */
-#define SDL_HINT_JOYSTICK_MFI "SDL_JOYSTICK_MFI"
 
  /**
   *  \brief  A variable controlling whether the RAWINPUT joystick drivers should be used for better handling XInput-capable devices.
